2addac699ee3bc1432d9223964b5e6c8c7e63b46
Loko.Elena <loko.elena@mail.ru>
2016-03-10 10:23:21 +0600

    Checking of the A argument

--- equation.c
+++ equation.c
@@ -7,5 +7,9 @@ int main()
       float a, b, c, x1, x2, d;
       printf ("input a,b,c separated by space, then press enter \n");
       scanf ("%f%f%f", &a, &b, &c);
+      if(a == 0) {
+            fprintf(stderr,"A can't be equal 0\n");
+            exit(EXIT_FAILURE);
+      }
       return 0;
 }
